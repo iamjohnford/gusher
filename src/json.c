6384d8d4df8c88f81465c2d2cf3459b60fe89fd6
Pete Yadlowsky <pmy@virginia.edu>
2013-02-20 15:16:07 -0500

    general code streamlining

--- src/json.c
+++ src/json.c
@@ -18,23 +18,21 @@ static json_t *json_build(SCM obj) {
 		buf = scm_to_locale_string(obj);
 		jobj = json_string(buf);
 		free(buf);
-		return jobj;
 		}
-	if (scm_is_symbol(obj)) {
+	else if (scm_is_symbol(obj)) {
 		buf = scm_to_locale_string(scm_symbol_to_string(obj));
 		jobj = json_string(buf);
 		free(buf);
-		return jobj;
 		}
-	if (scm_boolean_p(obj) == SCM_BOOL_T)
-		return (obj == SCM_BOOL_T ? json_true() : json_false());
-	if (scm_null_p(obj) == SCM_BOOL_T)
-		return json_null();
-	if (scm_integer_p(obj) == SCM_BOOL_T)
-		return json_integer(scm_to_int(obj));
-	if (scm_real_p(obj) == SCM_BOOL_T)
-		return json_real(scm_to_double(obj));
-	if ((scm_list_p(obj) == SCM_BOOL_T) &&
+	else if (scm_boolean_p(obj) == SCM_BOOL_T)
+		jobj = (obj == SCM_BOOL_T ? json_true() : json_false());
+	else if (scm_null_p(obj) == SCM_BOOL_T)
+		jobj = json_null();
+	else if (scm_integer_p(obj) == SCM_BOOL_T)
+		jobj = json_integer(scm_to_int(obj));
+	else if (scm_real_p(obj) == SCM_BOOL_T)
+		jobj = json_real(scm_to_double(obj));
+	else if ((scm_list_p(obj) == SCM_BOOL_T) &&
 			(scm_pair_p(SCM_CAR(obj)) == SCM_BOOL_T)) {
 		char *key;
 		jobj = json_object();
@@ -45,23 +43,20 @@ static json_t *json_build(SCM obj) {
 					json_build(SCM_CDR(pair)));
 			free(key);
 			}
-		return jobj;
 		}
-	if (scm_list_p(obj) == SCM_BOOL_T) {
+	else if (scm_list_p(obj) == SCM_BOOL_T) {
 		jobj = json_array();
 		for (node = obj; node != SCM_EOL; node = SCM_CDR(node))
 			json_array_append(jobj, json_build(SCM_CAR(node)));
-		return jobj;
 		}
-	return json_null();
+	else jobj = json_null();
+	return jobj;
 	}
 
 static SCM json_encode(SCM obj) {
 	char *buf;
 	SCM string;
-	json_t *root;
-	root = json_build(obj);
-	buf = json_dumps(root, JSON_COMPACT);
+	buf = json_dumps(json_build(obj), JSON_COMPACT);
 	if (buf == NULL) {
 		fprintf(stderr, "JSON encode failed\n");
 		return SCM_BOOL_F;
