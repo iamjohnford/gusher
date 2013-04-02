268ea943b069208a262e0a847697481d26ce7f19
Pete Yadlowsky <pmy@virginia.edu>
2013-04-02 08:14:00 -0400

    process and return http headers as well as body
    Post-process body contingent on content-type; e.g. "text/json"
    decodes JSON body to s-expr.

--- src/http.c
+++ src/http.c
@@ -19,14 +19,22 @@
 #include <curl/curl.h>
 #include <curl/easy.h>
 #include <libguile.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "json.h"
+#include "log.h"
 
 typedef struct hnode {
 	CURL *handle;
 	struct hnode *next;
+	char domain[];
 	} HNODE;
 
 static HNODE *hpool = NULL;
 static SCM mutex;
+static SCM content_type;
+static SCM infix_grammar;
 
 static HNODE *new_handle() {
 	HNODE *node;
@@ -72,15 +80,52 @@ static size_t write_handler(void *buffer, size_t size,
 	return rsize;
 	}
 
+static char *downcase(char *buf) {
+	char *pt;
+	for (pt = buf; *pt; pt++) *pt = tolower(*pt);
+	return buf;
+	}
+
+static size_t header_handler(void *data, size_t size, size_t n,
+									void *userp) {
+	char *buf, *pt, *value;
+	size_t rsize;
+	SCM sym, val;
+	buf = (char *)data;
+	rsize = size * n;
+	buf[rsize] = '\0';
+	if ((pt = index(buf, ':')) == NULL) return rsize;
+	*pt++ = '\0';
+	while (isspace(*pt)) pt++;
+	sym = scm_from_locale_symbol(downcase(buf));
+	value = pt;
+	pt = index(value, '\r');
+	if (pt == NULL) pt = index(value, '\n');
+	*pt = '\0';
+	val = scm_from_locale_string(value);
+	*((SCM *)userp) = scm_acons(sym, val, *((SCM *)userp));
+	return rsize;
+	}
+
+static SCM process_body(SCM headers, SCM body) {
+	char *ctype;
+	ctype = scm_to_locale_string(scm_assq_ref(headers, content_type));
+	if (strcmp(ctype, "text/json") == 0) body = json_decode(body);
+	free(ctype);
+	scm_remember_upto_here_1(body);
+	return body;
+	}
+
 static SCM http_get(SCM url) {
 	HNODE *hnode;
 	CURL *handle;
-	char *surl;
+	char *surl, errbuf[CURL_ERROR_SIZE];
 	CURLcode res;
-	SCM chunks, glue, grammar;
+	SCM chunks, glue, body, headers, reply;
 	hnode = get_handle();
 	handle = hnode->handle;
 	chunks = SCM_EOL;
+	headers = SCM_EOL;
 	surl = scm_to_locale_string(url);
 	curl_easy_setopt(handle, CURLOPT_URL, surl);
 	/*curl_easy_setopt(handle, CURLOPT_HEADER, 1);
@@ -88,17 +133,28 @@ static SCM http_get(SCM url) {
 	curl_easy_setopt(handle, CURLOPT_NOSIGNAL, 1);
 	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_handler);
 	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&chunks);
+	curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, header_handler);
+	curl_easy_setopt(handle, CURLOPT_HEADERDATA, (void *)&headers);
+	curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, errbuf);
 	res = curl_easy_perform(handle);
 	free(surl);
-	fprintf(stderr, "res: %d\n", res);
 	release_handle(hnode);
+	if (res != 0) {
+		log_msg("http-get error: %s\n", errbuf);
+		return SCM_BOOL_F;
+		}
 	glue = scm_from_locale_string("");
-	grammar = scm_from_locale_symbol("infix");
-	return scm_string_join(chunks, glue, grammar);
+	body = scm_string_join(scm_reverse(chunks), glue, infix_grammar);
+	reply = scm_cons(headers, process_body(headers, body));
+	scm_remember_upto_here_2(chunks, headers);
+	scm_remember_upto_here_2(body, reply);
+	return reply;
 	}
 
 void init_http() {
 	curl_global_init(CURL_GLOBAL_ALL);
+	content_type = scm_from_locale_symbol("content-type");
+	infix_grammar = scm_from_locale_symbol("infix");
 	mutex = scm_make_mutex();
 	scm_c_define_gsubr("http-get", 1, 0, 0, http_get);
 	}
