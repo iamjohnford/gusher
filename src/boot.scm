8310f0eefbe62a8d436c0d883213522c3306e5df
Pete Yadlowsky <pmy@linux.com>
2015-11-26 09:13:01 -0500

    add postgres query helpers

--- src/boot.scm
+++ src/boot.scm
@@ -1,21 +1,39 @@
 
+(use-modules (ice-9 format))
+
 (add-to-load-path gusher-root)
 
+; responders
 (define (http-html path responder)
+	; HTML response
 	(http path
 		(lambda (req)
 			(simple-response "text/html; charset=UTF-8" (responder req)))))
 (define (http-xml path responder)
+	; XML responsee
 	(http path
 		(lambda (req)
 			(simple-response "text/xml; charset=UTF-8" (responder req)))))
 (define (http-text path responder)
+	; plain text response
 	(http path
 		(lambda (req)
 			(simple-response "text/plain; charset=UTF-8" (responder req)))))
 (define (http-json path responder)
+	; JSON response
 	(http path
 		(lambda (req)
 			(let ([body (json-encode (responder req))])
 				(json-response body)))))
 
+; postgres helpers
+(define (pg-query dbh query args)
+	; safely plug SQL query parameters into query template,
+	; to be used with postgres calls below
+	(apply format
+		(append (list #f query)
+			(map (lambda (item) (pg-format dbh item)) args))))
+(define (pg-query-exec dbh query . args)
+	(pg-exec dbh (pg-query dbh query args)))
+(define (pg-query-one-row dbh query . args)
+	(pg-one-row dbh (pg-query dbh query args)))
