269b5fb1fbdbc2d1563b5af915f255049237e729
Pete Yadlowsky <pmy@linux.com>
2016-01-01 18:18:41 -0500

    add session-unset
    to remove session variables

--- src/session.scm
+++ src/session.scm
@@ -1,7 +1,7 @@
 (define-module (gusher session)
 	#:use-module (guile-user)
 	#:use-module (gusher kv)
-	#:export (session-get session-set)
+	#:export (session-get session-set session-unset)
 	)
 
 (define (session-key http-request)
@@ -21,6 +21,16 @@
 			[session-raw (kv-get kvh sess-key)]
 			[session (or (json-decode (or session-raw "")) '())])
 		(set! session (assq-set! session key value))
+		(set! session
+			(assq-set! session 'stamp (to-i (time-epoch (time-now)))))
+		(kv-set kvh sess-key (json-encode session))
+		(kv-close kvh)))
+(define (session-unset http-request key)
+	(let* ([kvh (kv-open "sessions")]
+			[sess-key (session-key http-request)]
+			[session-raw (kv-get kvh sess-key)]
+			[session (or (json-decode (or session-raw "")) '())])
+		(set! session (assq-remove! session key))
 		(set! session
 			(assq-set! session 'stamp (to-i (time-epoch (time-now)))))
 		(kv-set kvh sess-key (json-encode session))
