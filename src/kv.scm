932c3cd406c3c53b1ada5a518607d6ec1818814d
Pete Yadlowsky <pmy@linux.com>
2016-10-27 22:42:57 -0400

    embed SQL queries in define blocks

--- src/kv.scm
+++ src/kv.scm
@@ -2,65 +2,65 @@
 
 (define-module (gusher kv)
 	#:use-module (guile-user)
+	#:use-module (gusher misc)
 	#:use-module (gusher postgres)
 	#:export
 		(kv-open kv-close kv-get kv-set kv-exists kv-count kv-keys kv-del))
 
 (define (kv-open namespace . other) (cons (pg-open) namespace))
 (define (kv-close kv-handle) (pg-close (car kv-handle)))
-(define kv-get-query "\
-select value
-	from _kv_
-	where (namespace=~a)
-	and (key=~a)")
-(define (kv-get kv-handle key)
-	(let ([row (pg-one-row (car kv-handle) kv-get-query
-					(cdr kv-handle) key)])
-		(pg-cell row 'value)))
-(define kv-set-update-query "\
-update _kv_ set
-	value=~a
-	where (namespace=~a)
-	and (key=~a)")
-(define kv-set-insert-query "\
-insert into _kv_ (namespace, key, value)
-	values (~a, ~a, ~a)")
-(define (kv-set kv-handle key value)
-	(let ([res
-				(pg-exec (car kv-handle) kv-set-update-query
-					value (cdr kv-handle) key)])
-		(when (< (pg-cmd-tuples res) 1)
-			(pg-exec (car kv-handle) kv-set-insert-query
-				(cdr kv-handle) key value))))
-(define kv-key-exists-query "\
-select key
-	from _kv_
-	where (namespace=~a)
-	and (key=~a)")
-(define (kv-exists kv-handle key)
-	(let ([row (pg-one-row (car kv-handle) kv-key-exists-query
-					(cdr kv-handle) key)])
-		(not (not row))))
-(define kv-count-query "\
-select count(*) as n
-	from _kv_
-	where (namespace=~a)")
-(define (kv-count kv-handle)
-	(let ([row (pg-one-row (car kv-handle) kv-count-query
-					(cdr kv-handle))])
-		(pg-cell row 'n)))
-(define kv-keys-query "\
-select key
-	from _kv_
-	where (namespace=~a)")
-(define (kv-keys kv-handle)
-	(let ([res (pg-exec (car kv-handle) kv-keys-query
-					(cdr kv-handle))])
-		(pg-map-rows res (lambda (row) (pg-cell row 'key)))))
-(define kv-delete-query "\
-delete from _kv_
-	where (namespace=~a)
-	and (key=~a)")
-(define (kv-del kv-handle key)
-	(pg-exec (car kv-handle) kv-delete-query
-		(cdr kv-handle) key))
+(define kv-get
+	(let ([query
+				(deflate "select value
+					from _kv_
+					where (namespace=~a)
+					and (key=~a)")])
+		(lambda (kv-handle key)
+			(let ([row (pg-one-row (car kv-handle) query
+							(cdr kv-handle) key)])
+				(pg-cell row 'value)))))
+(define kv-set
+	(let ([update-query
+				(deflate "update _kv_ set
+					value=~a
+					where (namespace=~a)
+					and (key=~a)")]
+			[insert-query
+				(deflate "insert into _kv_ (namespace, key, value)
+					values (~a, ~a, ~a)")])
+		(lambda (kv-handle key value)
+			(let ([res
+						(pg-exec (car kv-handle) update-query
+							value (cdr kv-handle) key)])
+				(when (< (pg-cmd-tuples res) 1)
+					(pg-exec (car kv-handle) insert-query
+						(cdr kv-handle) key value))))))
+(define kv-exists
+	(let ([query
+			(deflate "select key
+					from _kv_
+					where (namespace=~a)
+					and (key=~a)")])
+		(lambda (kv-handle key)
+			(let ([row (pg-one-row (car kv-handle) query
+							(cdr kv-handle) key)])
+				(not (not row))))))
+(define kv-count
+	(let ([query
+				(deflate "select count(*) as n
+					from _kv_
+					where (namespace=~a)")])
+		(lambda (kv-handle)
+			(let ([row (pg-one-row (car kv-handle) query
+							(cdr kv-handle))])
+				(pg-cell row 'n)))))
+(define kv-keys
+	(let ([query "select key from _kv_ where (namespace=~a)"])
+		(lambda (kv-handle)
+			(let ([res (pg-exec (car kv-handle) query (cdr kv-handle))])
+				(pg-map-rows res (lambda (row) (pg-cell row 'key)))))))
+(define kv-del
+	(let ([query "delete from _kv_ where (namespace=~a) and (key=~a)"])
+		(lambda (kv-handle key)
+			(pg-exec (car kv-handle) query
+				(cdr kv-handle) key))))
